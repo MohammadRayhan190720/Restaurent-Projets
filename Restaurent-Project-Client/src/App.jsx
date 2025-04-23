07e0c303e85c7cddb6549562ed9739a9aa404bec
MohammadRayhan190720 <rafirayhan260@gmail.com>
2025-04-24 01:18:54 +0600

    Client Project Setup Done

---
+++ Restaurent-Project-Client/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1 className='text-4xl bg-green-900 text-center'>Restuarent Project</h1>
+     
+
+    </>
+  )
+}
+
+export default App
