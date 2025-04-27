11ab84f5769b1c54c6d6917ca48928efa567cda3
MohammadRayhan190720 <rafirayhan260@gmail.com>
2025-04-28 00:19:26 +0600

    added more info

--- Restaurent-Project-Client/src/Layouts/MainLayouts.jsx
+++ Restaurent-Project-Client/src/Layouts/MainLayouts.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const MainLayouts = () => {
   return (
-    <div>
+    <div className="font-fontInter max-w-7xl mx-auto border">
       <Outlet></Outlet>
       
       
