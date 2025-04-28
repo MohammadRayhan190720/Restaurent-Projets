c5a8b2c786edd21c2751021857c5f1f785da0378
MohammadRayhan190720 <rafirayhan260@gmail.com>
2025-04-28 23:53:35 +0600

    added dixed navbar

--- Restaurent-Project-Client/src/components/shared/Navbar.jsx
+++ Restaurent-Project-Client/src/components/shared/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar fixed z-10 bg-black opacity-50  text-white max-w-7xl mx-auto">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -48,15 +48,12 @@ const Navbar = () => {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
             {menu}
-            
           </ul>
         </div>
         <a className="btn btn-ghost text-xl">Bristro Boss Resturant</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          {menu}
-        </ul>
+        <ul className="menu menu-horizontal px-1">{menu}</ul>
       </div>
       <div className="navbar-end">
         <a className="btn">Button</a>
