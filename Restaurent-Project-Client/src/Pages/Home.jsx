ef88a7d56d1ff974b5ccf2742ef0234a00906441
MohammadRayhan190720 <rafirayhan260@gmail.com>
2025-07-24 00:28:16 +0600

    added popular menu

--- Restaurent-Project-Client/src/Pages/Home.jsx
+++ Restaurent-Project-Client/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../components/Banner';
 import Category from '../components/Category';
 import HomeBg from '../components/HomeBg';
+import PopularMenu from '../components/PopularMenu';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
      <Banner></Banner>
      <Category></Category>
      <HomeBg></HomeBg>
+     <PopularMenu></PopularMenu>
       
     </div>
   );
---
+++ Restaurent-Project-Client/src/components/PopularMenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SectionTitle from './shared/SectionTitle';
+
+const PopularMenu = () => {
+  return (
+    <div>
+      <SectionTitle subHeading={"Check it out"} heading={"Popular Menu"}>
+
+      </SectionTitle>
+      
+    </div>
+  );
+};
+
+export default PopularMenu;
