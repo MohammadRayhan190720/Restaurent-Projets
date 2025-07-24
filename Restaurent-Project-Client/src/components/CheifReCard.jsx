1308c2b0148e5736078f1644d549b5b1cb6328a4
MohammadRayhan190720 <rafirayhan260@gmail.com>
2025-07-25 02:07:29 +0600

    added testmonial section

--- Restaurent-Project-Client/src/components/CheifReCard.jsx
+++ Restaurent-Project-Client/src/components/CheifReCard.jsx
@@ -1,14 +1,10 @@
 import cardimage from '../assets/home/slide1.jpg'
 const CheifReCard = () => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-10'>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-10">
       <div className="card bg-base-100  shadow-xl">
         <figure className="px-10 pt-10">
-          <img
-            src={cardimage}
-            alt="Shoes"
-            className="rounded-xl"
-          />
+          <img src={cardimage} alt="Shoes" className="rounded-xl" />
         </figure>
         <div className="card-body items-center text-center">
           <h2 className="card-title">Card Title</h2>
@@ -17,17 +13,15 @@ const CheifReCard = () => {
             are title and actions parts
           </p>
           <div className="card-actions">
-            <button className="btn btn-primary">Add To Cart</button>
+            <button className="btn btn-outline border-0 border-b-4 mt-4">
+              Order Now
+            </button>{" "}
           </div>
         </div>
       </div>
       <div className="card bg-base-100  shadow-xl">
         <figure className="px-10 pt-10">
-          <img
-            src={cardimage}
-            alt="Shoes"
-            className="rounded-xl"
-          />
+          <img src={cardimage} alt="Shoes" className="rounded-xl" />
         </figure>
         <div className="card-body items-center text-center">
           <h2 className="card-title">Card Title</h2>
@@ -36,17 +30,15 @@ const CheifReCard = () => {
             are title and actions parts
           </p>
           <div className="card-actions">
-            <button className="btn btn-primary">Add To Cart</button>
+            <button className="btn btn-outline border-0 border-b-4 mt-4">
+              Order Now
+            </button>{" "}
           </div>
         </div>
       </div>
       <div className="card bg-base-100 shadow-xl">
         <figure className="px-10 pt-10">
-          <img
-            src={cardimage}
-            alt="Shoes"
-            className="rounded-xl"
-          />
+          <img src={cardimage} alt="Shoes" className="rounded-xl" />
         </figure>
         <div className="card-body items-center text-center">
           <h2 className="card-title">Card Title</h2>
@@ -55,7 +47,9 @@ const CheifReCard = () => {
             are title and actions parts
           </p>
           <div className="card-actions">
-            <button className="btn btn-primary">Add To Cart</button>
+            <button className="btn btn-outline border-0 border-b-4 mt-4">
+              Order Now
+            </button>{" "}
           </div>
         </div>
       </div>
