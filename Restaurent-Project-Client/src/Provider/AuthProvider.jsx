dc17da3a2f2ff3f23aa49aaa63f7da0fe6f8faaf
MohammadRayhan190720 <rafirayhan260@gmail.com>
2025-09-23 00:37:22 +0600

    added Firebase setup

---
+++ Restaurent-Project-Client/src/Provider/AuthProvider.jsx
@@ -0,0 +1,25 @@
+import React, { createContext } from 'react';
+import { app } from '../firebase/firbase.config';
+import { getAuth } from 'firebase/auth';
+
+
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+  const [user,setUser] = useState(null);
+  const [loading,setLoading] = useState(true);
+
+  const authInfo = {
+    user,
+    loading,
+  }
+  return (
+    <AuthContext.Provider value={authInfo}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
