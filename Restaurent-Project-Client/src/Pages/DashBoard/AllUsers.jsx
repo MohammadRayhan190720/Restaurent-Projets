e802f129bf704ea1e8317102867758816f014bed
MohammadRayhan190720 <rafirayhan260@gmail.com>
2025-10-20 07:57:17 +0600

    added jwt for More Security

--- Restaurent-Project-Client/src/Pages/DashBoard/AllUsers.jsx
+++ Restaurent-Project-Client/src/Pages/DashBoard/AllUsers.jsx
@@ -14,6 +14,24 @@ const AllUsers = () => {
     }
   })
 
+  
+  const handleMakeAdmin = user =>{
+    axiosSecure.patch(`/users/admin/${user._id}`)
+    .then(res =>{
+      // console.log(res.data)
+      if(res.data.modifiedCount > 0){
+        refetch();
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: `${user.name} is an Admin Now!`,
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+  })
+}
+
   const handleDelete = id =>{
     // console.log(id)
     
--- Resturent-Project-Server/index.js
+++ Resturent-Project-Server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 require("dotenv").config({ debug: true });
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
