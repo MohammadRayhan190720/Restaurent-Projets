a72f9f72e0bd614814b980d89b773e1037c37669
MohammadRayhan190720 <rafirayhan260@gmail.com>
2025-10-20 16:12:19 +0600

    added admin route

--- Resturent-Project-Server/index.js
+++ Resturent-Project-Server/index.js
@@ -65,11 +65,22 @@ async function run() {
 
     }
 
+    const verifyAdmin = async (req,res,next) =>{
+      const email = req.params.email;
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message: 'forbidden access'});
+      }
+      next();
+    }
+
     //users collection api
 
 
 
-    app.get('/users', verifyToken, async(req,res) =>{
+    app.get('/users', verifyToken,verifyAdmin, async(req,res) =>{
       const result = await userCollection.find().toArray();
       res.send(result);
     })
@@ -99,7 +110,7 @@ async function run() {
     })
 
     // set admin role'
-    app.patch('/users/admin/:id',async(req,res) =>{
+    app.patch('/users/admin/:id',verifyToken, verifyAdmin, async(req,res) =>{
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)};
       const updatedDoc = {
@@ -111,7 +122,7 @@ async function run() {
       res.send(result);
     })
 
-    app.delete("/users/:id",async(req,res) =>{
+    app.delete("/users/:id",verifyToken, verifyAdmin, async(req,res) =>{
       const id = req.params.id;
       const query = {_id : new ObjectId(id)};
       const result = await userCollection.deleteOne(query);
